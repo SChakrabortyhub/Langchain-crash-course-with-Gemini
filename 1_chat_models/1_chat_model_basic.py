c0f7a8bf1880820f00ed4e88494565120a39485c
[Shatadru Chakraborty] <[chakrabortyshatadru93@gmail.com]>
2025-04-29 16:03:50 +0530

    First commit

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -1,18 +1,27 @@
 # Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
 # OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
 
+import os
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Load environment variables from .env
 load_dotenv()
 
-# Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+#Read the API key from environment
+#google_api_key = os.getenv("GOOGLE_API_KEY")
+
+# Create a ChatGoogleGenerativeAI model
+
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.7)
 
 # Invoke the model with a message
-result = model.invoke("What is 81 divided by 9?")
-print("Full result:")
-print(result)
-print("Content only:")
-print(result.content)
+try:
+    result = model.invoke("What is 81 divided by 9?")
+    print("Full result:")
+    print(result)
+    print("Content only:")
+    print(result.content)
+except Exception as e:
+    print(f"Request failed immediately: {e}")
+
