f8b38b9f7003af1a8e790b3186a8cd836b11c1be
[Shatadru Chakraborty] <[chakrabortyshatadru93@gmail.com]>
2025-04-29 16:20:19 +0530

    Second Commit

--- 1_chat_models/2_chat_model_basic_conversation.py
+++ 1_chat_models/2_chat_model_basic_conversation.py
@@ -1,16 +1,16 @@
 from dotenv import load_dotenv
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Load environment variables from .env
 load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.7)
 
 # SystemMessage:
-#   Message for priming AI behavior, usually passed in as the first of a sequenc of input messages.
-# HumanMessagse:
+#   Message for priming AI behavior, usually passed in as the first of a sequence of input messages.
+# HumanMessage:
 #   Message from a human to the AI model.
 messages = [
     SystemMessage(content="Solve the following math problems"),
--- 1_chat_models/3_chat_model_alternatives.py
+++ 1_chat_models/3_chat_model_alternatives.py
@@ -1,6 +1,4 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_anthropic import ChatAnthropic
-from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 
@@ -11,27 +9,6 @@ messages = [
     SystemMessage(content="Solve the following math problems"),
     HumanMessage(content="What is 81 divided by 9?"),
 ]
-
-
-# ---- LangChain OpenAI Chat Model Example ----
-
-# Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
-
-# Invoke the model with messages
-result = model.invoke(messages)
-print(f"Answer from OpenAI: {result.content}")
-
-
-# ---- Anthropic Chat Model Example ----
-
-# Create a Anthropic model
-# Anthropic models: https://docs.anthropic.com/en/docs/models-overview
-model = ChatAnthropic(model="claude-3-opus-20240229")
-
-result = model.invoke(messages)
-print(f"Answer from Anthropic: {result.content}")
-
 
 # ---- Google Chat Model Example ----
 
